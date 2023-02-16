da7a4870b27170fdd5aa8a2ac168b1c5f91c8d5d
Muhammad Adil Mehmood <iamadilmalick@gmail.com>
2023-02-16 11:10:14 +0500

    adding initial iOS project

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Muhammad Adil Mehmood on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
